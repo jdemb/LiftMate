aebafc371371379aeb53845751e177bb47550a59
Jdemb <jentszej@gmail.com>
2026-05-26 18:57:29 +0200

    api: prepare Azure smoke deploy

--- apps/api/LiftMate.Api/Program.cs
+++ apps/api/LiftMate.Api/Program.cs
@@ -3,16 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.MapGet("/health", () => Results.Ok(new { status = "ok" }))
+    .WithName("HealthCheck");
 
 var summaries = new[]
 {
